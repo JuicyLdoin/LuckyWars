a65888405ec38e84233e966d662285d1f355eec9
JuicyLdoin <ldoinplay224@gmail.com>
2021-11-20 12:53:53 +0200

    Added LuckyBlock

---
+++ src/ua/Ldoin/JuicyLuckyWars/Game/LuckyBlock/LuckyBlock.java
@@ -0,0 +1,126 @@
+package ua.Ldoin.JuicyLuckyWars.Game.LuckyBlock;
+
+import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import ua.Ldoin.JuicyLuckyWars.Main.Main;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LuckyBlock {
+
+    public LuckyBlock(short data, String displayName, String head, int itemChance, int trapChance) {
+
+        this.data = data;
+        this.displayName = displayName;
+
+        this.head = head;
+
+        this.itemChance = itemChance;
+        this.trapChance = trapChance;
+
+    }
+
+    private static final Map<String, LuckyBlock> luckyBlocks = new HashMap<>();
+
+    public static LuckyBlock getLuckyBlockByName(String name) {
+
+        return luckyBlocks.get(name);
+
+    }
+    public static LuckyBlock getLuckyBlockByData(short data) {
+
+        for (LuckyBlock block : luckyBlocks.values())
+            if (block.getData() == data)
+                return block;
+
+        return null;
+
+    }
+
+    private final short data;
+    private final String displayName;
+
+    private final String head;
+
+    private final int itemChance;
+    private final int trapChance;
+
+    public short getData() {
+
+        return data;
+
+    }
+
+    public String getDisplayName() {
+
+        return displayName;
+
+    }
+
+    public String getHead() {
+
+        return head;
+
+    }
+
+    public int getItemChance() {
+
+        return itemChance;
+
+    }
+
+    public int getTrapChance() {
+
+        return trapChance;
+
+    }
+
+    public ItemStack getItemStack() {
+
+        ItemStack item = new ItemStack(Material.SKULL_ITEM);
+        ItemMeta meta = item.getItemMeta();
+
+        meta.setDisplayName(displayName.replace("&", "§"));
+
+        List<String> lore = new ArrayList<>();
+
+        lore.add("");
+        lore.add("§fШанс удачи: §a" + itemChance + "%");
+        lore.add("§fШанс неудачи: §c" + trapChance + "%");
+        lore.add("");
+
+        meta.setLore(lore);
+
+        item.setItemMeta(meta);
+
+        return item;
+
+    }
+
+    public static void init() {
+
+        FileConfiguration config = Main.plugin.getConfig();
+
+        for (String block : config.getConfigurationSection("LuckyBlocks").getKeys(false)) {
+
+            short data = Short.parseShort(config.getString("LuckyBlocks." + block + ".data"));
+            String displayName = config.getString("LuckyBlocks." + block + ".name");
+
+            String head = config.getString("LuckyBlocks." + block + ".head");
+
+            int itemChance = config.getInt("LuckyBlocks." + block + ".items.chance");
+            int trapChance = config.getInt("LuckyBlocks." + block + ".trap.chance");
+
+            luckyBlocks.put(block, new LuckyBlock(data, displayName, head, itemChance, trapChance));
+
+        }
+
+        Main.sendMessageToConsole(Main.prefix + "&fLoaded &e" + luckyBlocks.size() + " &fLuckyBlocks!");
+
+    }
+}
